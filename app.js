d9f5cdec7aab4fc7f49cd58a7e5e21a7551c6055
yousun22 <hanlddang22@gmail.com>
2023-10-22 16:15:43 +0900

    백앤드에서 로그인 기능 구현

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
-"user strict"
+"use strict"
 
 // 모듈
 const express = require("express");
+const bodyParser =require("body-parser");
 const app = express();
-
-
 
 //라우팅
 const home = require("./src/routes/home");
@@ -16,7 +15,8 @@ app.set("view engine", "ejs");
 
 //app.use(express.static(`{$__dirname}/src/public`));
 app.use(express.static(`${__dirname}/src/public`));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use("/",home);//use는 미들웨어를 등록해주는 메서드
 
 module.exports =app;
